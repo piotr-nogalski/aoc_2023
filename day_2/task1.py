7ad605426b62110680ad636a7c1e1b6123f2da64
Piotr Nogalski <111290102+piotr-nogalski@users.noreply.github.com>
2023-12-05 16:51:24 +0100

    Task 2.1 Solved

--- day_2/task1.py
+++ day_2/task1.py
@@ -1,10 +1,67 @@
 import time
-import pandas as pds
+
 # Variables
-File = open("sample.txt", "r")
-sum =0
+File = open("data.txt", "r")
+sum = 0
+positive = 0
+game = []
+result = []
+legal_values = { 'r':12,
+                 'g':13,
+                 'b':14}
+change = { ':':';',
+           ';':',',
+           'Game ':"",
+           " red":"r",
+           " blue":"b",
+           " green":"g",
+           "\n":"",
+           " ":""}
+
+# Algorithm
 start = time.time()
-# Code to enter here, probably gonna transfer data to csv file
+# Modife data for futrhter operations, change values listed in change dictionary
+for line in File:
+    for x,y in change.items():
+        line=line.replace(x,y)
+    game = line.split(",")
+
+# Check for every game if it fits legal requierements
+# Try/except helps in cases when ball number contains one or two digits ex:. 1b, 11b
+# Iterate through legal values dictionary reduce if/else statements
+# game[value][2] represents color of two digit numbered ball ex:. 11b
+# game[value][1] represents color of one digit numbered ball ex:. 2g
+    for value in range(1,len(game)):
+        # Tries for two digit balls number
+        try:
+            for color, legal_value in legal_values.items():
+                if game[value][2] == color:
+                    if int(game[value][0:2]) <= legal_value:
+                        positive+=1
+                    else:
+                        continue
+            if positive == len(game)-1:
+                result.append(int(game[0]))
+        # When two digit balls number fails checks for one digit number of balls
+        except IndexError:
+            for color, legal_value in legal_values.items():
+                if game[value][1] == color:
+                    if int(game[value][0]) <= legal_value:
+                        positive+=1
+                    else:
+                        continue
+            # Checks if all configurations were possible
+            if positive == len(game)-1:
+                result.append(int(game[0]))
+    # Resets configuration check
+    positive = 0
+
+# Add final results for answer
+for value in result:
+    sum += value
 end = time.time()
-print(sum)
+
+# Prompt with result
+print(result)
+print("Answer is equal: {}".format(sum))
 print("Execution time was: {} ".format(end - start))
