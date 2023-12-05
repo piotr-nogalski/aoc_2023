3ac4cc452d5bedd601021fa95a584f77b9023a25
Piotr Nogalski <111290102+piotr-nogalski@users.noreply.github.com>
2023-12-05 18:39:19 +0100

    Task 2.2 Solved

--- day_2/task2.py
+++ day_2/task2.py
@@ -3,62 +3,68 @@ import time
 # Variables
 File = open("data.txt", "r")
 sum = 0
+ans = 1
+game = []
+result = []
+max_values = {   'r':1,
+                 'g':1,
+                 'b':1}
+change = { ':':';',
+           ';':',',
+           'Game ':"",
+           " red":"r",
+           " blue":"b",
+           " green":"g",
+           "\n":"",
+           " ":""}
+
+# Algorithm
 start = time.time()
-# Prefixes and postfixes provide fix for situation when two numbers share one letter ex:. eightwo
-written_nums = {"zero":0,
-                "one":1,
-                "two":2,
-                "three":3,
-                "four":4,
-                "five":5,
-                "six":6,
-                "seven":7,
-                "eight":8,
-                "nine":9}
-# Read file with data line by line and filter out digits
+# Modife data for futrhter operations, change values listed in change dictionary
 for line in File:
-    num = []
-# Iterate through dictionary written_nums to change every written number to its digital form
-    for s, d in written_nums.items():
-        if line.find(s) >= 0:
-            line = line.replace(s, str(d))
-    for value in line:
-        if value.isdigit():
-            num.append(int(value))
-    # Format digits to coordinates, by instruction coordinates are first and last digit in each line
-    # try except for blank lines (blank "num" variable)
-    try:
-        coordinate = str(num[0])
-        coordinate += str(num[-1])
-    except IndexError:
-        coordinate = 0
-    sum += int(coordinate)
+    for x,y in change.items():
+        line=line.replace(x,y)
+    game = line.split(",")
 
+# Check for every game for its maximu number of each ball color
+# Try/except helps in cases when ball number contains one or two digits ex:. 1b, 11b
+# Iterate through legal values dictionary reduce if/else statements
+# game[value][2] represents color of two digit numbered ball ex:. 11b
+# game[value][1] represents color of one digit numbered ball ex:. 2g
+    for value in range(1,len(game)):
+        # Tries for two digit balls number
+        try:
+            for color, max_value in max_values.items():
+                if game[value][2] == color:
+                    if int(game[value][0:2]) >= max_value:
+                        max_values[color] = int(game[value][0:2])
+                    else:
+                        continue
+
+        # When two digit balls number fails checks for one digit number of balls
+        except IndexError:
+            for color, max_value in max_values.items():
+                if game[value][1] == color:
+                    if int(game[value][0]) >= max_value:
+                        max_values[color] = int(game[value][0])
+                    else:
+                        continue
+    # Multiplies max color numbers for power of set
+    for color, max_value in max_values.items():
+     ans *= max_value
+
+    result.append(ans)
+    for color in max_values:
+        max_values[color] = 1
+    ans = 1
+
+# Add final results for answer
+
+for value in result:
+    sum += value
 end = time.time()
-print(sum)
+
+# Prompt with result
+print(result)
+print("Answer is equal: {}".format(sum))
 print("Execution time was: {} ".format(end - start))
-
-
-# Tests executed on sample.txt file
-
-"""sample = open("sample.txt", "r")
-test_array = []
-bruv_sum = 0
-for line in sample:
-    test_array = []
-    print(line)
-    for s,d in written_nums.items():
-        if line.find(s) >= 0:
-            line = line.replace(s,str(d))
-    print(line)
-    for value in line:
-        if value.isdigit():
-            test_array.append(int(value))
-
-    bruv = str(test_array[0])
-    bruv += str(test_array[-1])
-    bruv_sum += int(bruv)
-    print(test_array)
-
-print(bruv_sum)"""
-
