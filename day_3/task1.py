0d64c3ddf80720f7508be7dca8b2f313a2e5c764
Piotr Nogalski <111290102+piotr-nogalski@users.noreply.github.com>
2023-12-06 22:04:47 +0100

    Added algorithm that converts data.txt to two-dimensional array

--- day_3/task1.py
+++ day_3/task1.py
@@ -1,16 +1,36 @@
 import time
 
+# Variable
 file = open("sample.txt", "r")
 sum = 0
+row = 0
+result = []
 engine_map = []
 
+
+# Algorithm
 start = time.time()
+
 for line in file:
-    pass
+    engine_map.append([])
+    for value in line:
+        if value == '\n':
+            continue
+        else:
+            engine_map[row].append(value)
+    row+=1
+for column in range(0, len(engine_map)):
+    for row in range(0, len(engine_map[column])):
+        if engine_map[column][row].isdigit():
+            temp = []
 
 
 
 end = time.time()
+
+# Answer Prompt
+print(engine_map)
+print(result)
 
 print("Answer is: {}".format(sum))
 print("Working time is: {}".format(round(end-start, 5)))
