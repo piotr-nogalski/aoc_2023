b2dc0d56943e9da177d7434a291457572f4aca8e
Piotr Nogalski <111290102+piotr-nogalski@users.noreply.github.com>
2023-12-08 23:49:08 +0100

    Getting closer to part 3.2

--- day_3/task2.py
+++ day_3/task2.py
@@ -8,7 +8,7 @@ result = []
 engine_map = []
 temp = []
 symbols = '{}[]|;:<,>?"!@#$%^&*+\=-//'
-sum_item = ""
+check_gear = 0
 
 
 # Algorithm
@@ -36,22 +36,27 @@ for column in range(0, len(engine_map)):
                 for nei_co_1 in [-1,0,1]:
                     for nei_co_2 in [-1,0,1]:
                         for num in range(1,len(temp)+1):
-                            if engine_map[column + nei_co_1][row + nei_co_2 - num] == "*":
-                                print(temp)
+                            if engine_map[column + nei_co_1][row + nei_co_2 - num] == '*':
+                                result.append([int("".join(temp)),str(column + nei_co_1) + str(row + nei_co_2 - num)])
                                 temp = []
             except IndexError:
                 pass
             temp = []
 
-# Calculate final sum
-#for item in range(0,len(result)):
- #   for num in range(0,len(result[item])):
-  #      sum_item += result[item][num]
-   # sum += int(sum_item)
-    #sum_item = ""
-
+#Calculate final sum
+print(result)
+for items in range(0,len(result)):
+    try:
+        for awk in range(0,len(result)+1):
+            if result[items][1] == result[items+awk][1]:
+                print(result[items+awk][1], result[items+awk])
+                check_gear +=1
+        if check_gear == 2:
+            sum += result[items][0]*result[items+awk][0]
+    except IndexError:
+        pass
 end = time.time()
 
 # Answer Prompt
-#print("Answer is: {}".format(sum))
+print("Answer is: {}".format(sum))
 print("Working time is: {}".format(round(end-start, 5)))
